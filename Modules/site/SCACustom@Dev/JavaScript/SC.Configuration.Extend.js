b3aec7626583f8af882cb979aa6f9c79ac4b8fdd
Phillip Guevara <phillipguevara@Phillips-MBP.hsd1.or.comcast.net>
2020-09-02 14:02:18 -0700

    Continuing New Categories

--- Modules/site/SCACustom@Dev/JavaScript/SC.Configuration.Extend.js
+++ Modules/site/SCACustom@Dev/JavaScript/SC.Configuration.Extend.js
@@ -217,7 +217,7 @@ define(
                     }
                 },
                 {
-                    text: _('Men').translate(),
+                    text: _('Men\'s').translate(),
                     href: '/mens-gear',
                     'class': 'header-menu-level1-anchor'
                         // @property {Array<NavigationData>} categories
@@ -316,7 +316,7 @@ define(
                     ]
                 },
                 {
-                    text: _('Women').translate(),
+                    text: _('Women\'s').translate(),
                     href: '/womens-gear',
                     'class': 'header-menu-level1-anchor'
                         // @property {Array<NavigationData>} categories
@@ -387,7 +387,7 @@ define(
                     ]
                 },
                 {
-                    text: _('Luggage').translate(),
+                    text: _('Luggage & Bags').translate(),
                     href: '/luggage',
                     'class': 'header-menu-level1-anchor'
                         // @property {Array<NavigationData>} categories
